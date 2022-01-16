6456b25a261d049a15bf33f034178de016bd20c4
Rolkarolka <romanowskarolina@gmail.com>
2022-01-16 17:09:32 +0100

    append storage endpoint

--- server/src/main/kotlin/pishello/hello/api/StorageEndpoint.kt
+++ server/src/main/kotlin/pishello/hello/api/StorageEndpoint.kt
@@ -1,15 +1,23 @@
 package pishello.hello.api
 
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
-import pishello.hello.cloudStorage.PhotosStorage
-import java.util.*
+import org.springframework.web.bind.annotation.RestController
+import pishello.hello.persistence.cloudStorage.PhotosStorage
 
 
-class StorageEndpoint {
-    @GetMapping("/get_images")
-    fun get_images(): HashMap<String, String> {
-        val photosStorage = PhotosStorage()
-        val bucket = photosStorage.createNewBucket("test")
-        return photosStorage.downloadObject("strong-compiler-334811", "test", "pis2021/photos/business-card_1.jpg", "/")
+@RestController
+class StorageEndpoint(val photosStorage: PhotosStorage) {
+    @GetMapping("/text")
+    fun text(): String? {
+        photosStorage.writeText("Karo.txt", "a cookie monster")
+        photosStorage.save("Karo.txt", "/tmp/hmm.txt")
+        return photosStorage.readText("Karo.txt")
+    }
+
+    @GetMapping("/image", produces = [MediaType.IMAGE_JPEG_VALUE])
+    fun image(): ByteArray? {
+        photosStorage.save("photos/business-card_1.jpg", "/tmp/hmm.jpg")
+        return photosStorage.readImage("photos/business-card_1.jpg")
     }
 }
