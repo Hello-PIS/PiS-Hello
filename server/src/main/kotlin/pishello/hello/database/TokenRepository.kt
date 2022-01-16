8baad84fc9689961154b8fc1f2fc8df1af08f2c8
Rolkarolka <romanowskarolina@gmail.com>
2022-01-16 22:58:53 +0100

    HPIS-91 |really revert

--- server/src/main/kotlin/pishello/hello/database/TokenRepository.kt
+++
@@ -1,8 +0,0 @@
-package pishello.hello.database
-
-import org.springframework.data.repository.CrudRepository
-
-
-interface TokenRepository: CrudRepository<Token?, String?> {
-    fun findByName(name: User): Token?
-}
