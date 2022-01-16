964b18329cff0b52f28813df9a48de8df576777e
Rolkarolka <romanowskarolina@gmail.com>
2022-01-17 00:04:50 +0100

    HPIS-100 append sql card

--- server/src/main/sql/insert.sql
+++ server/src/main/sql/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO card(mode, path) VALUES('PRIVATE', 'test/photo1');
-INSERT INTO card(mode, path, category) VALUES('PUBLIC', 'test/photo2', 'lawyer');
-INSERT INTO card(mode, path, category) VALUES('PUBLIC', 'test/photo3', 'lawyer');
-INSERT INTO card(mode, path, category) VALUES('PUBLIC', 'test/photo4', 'cook');
+INSERT INTO cards(mode, path) VALUES('PRIVATE', 'test/photo1');
+INSERT INTO cards(mode, path, category) VALUES('PUBLIC', 'test/photo2', 'lawyer');
+INSERT INTO cards(mode, path, category) VALUES('PUBLIC', 'test/photo3', 'lawyer');
+INSERT INTO cards(mode, path, category) VALUES('PUBLIC', 'test/photo4', 'cook');
