7b33c6221b2dbed00eb163cb0ebf586b66f54c44
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-01-21 20:07:46 +0100

    HPIS-69 | added TODO

--- server/src/test/kotlin/pishello/hello/api/CardEndpointTests.kt
+++ server/src/test/kotlin/pishello/hello/api/CardEndpointTests.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
@@ -12,6 +11,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import pishello.hello.TestUtilities
 
+// TODO: change the bucket in GCS to not use the same as PROD
 
 @SpringBootTest
 @AutoConfigureMockMvc
