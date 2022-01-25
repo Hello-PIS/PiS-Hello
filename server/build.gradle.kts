0c026c7d5623b74e9f2ff86a9d6235025086bcda
ERoszczyk <01149741@pw.edu.pl>
2022-01-25 19:29:15 +0100

    HPIS-108 | Fix bugs

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,7 +2,6 @@ import groovy.lang.GroovyObject
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
-//apply(plugin = "com.android.application")
 apply(plugin = "com.google.android.gms.strict-version-matcher-plugin")
 
 plugins {
@@ -27,11 +26,9 @@ repositories {
 }
 
 dependencies {
-	implementation("androidx.camera:camera-camera2:1.0.0-alpha07")
 	val springBootVersion = "2.6.2"
 	val jacocoVersion = "0.8.7"
 	val sqliteJDBCVersion = "3.36.0.2"
-	val textRecognitionVersion = "17.0.1"
 
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -42,8 +39,6 @@ dependencies {
 	implementation("org.jacoco:org.jacoco.core:$jacocoVersion")
 	implementation(platform("com.google.cloud:libraries-bom:24.1.0"))
 	implementation("com.google.cloud:google-cloud-storage")
-	implementation( "com.google.android.gms:play-services-vision:19.0.0")
-	implementation("com.google.android.gms:play-services-mlkit-text-recognition:$textRecognitionVersion")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
 }
